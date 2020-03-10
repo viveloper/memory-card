3fd5ac1f759a838db4d61cd843dbcf97524b631d
viveloper <usik1234@gmail.com>
2020-03-10 11:13:26 +0900

    complete refactoring2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// DOM
 const cardsContainer = document.getElementById('cards-container');
+const navigation = document.getElementById('navigation');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const currentEl = document.getElementById('current');
@@ -10,128 +12,171 @@ const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
 
-// Keep track of current card
-let currentActiveCard = 0;
+// State
+const state = {
+  cards: getCards(),
+  activeCardIndex: 0
+};
 
-// Store DOM cards
-const cardsEl = [];
-
-const cardsData = getCardsData();
-
-// Create all cards
-function createCards() {
-  cardsData.forEach((item, index) => createCard(item, index));
+// Get card from local storage
+function getCards() {
+  const cards = localStorage.getItem('cards')
+    ? JSON.parse(localStorage.getItem('cards'))
+    : [];
+  return cards;
 }
 
-// Create single card in DOM
-function createCard(data, index) {
-  const card = document.createElement('div');
-  card.classList.add('card');
-  if (index === 0) {
-    card.classList.add('active');
-  }
+function init() {
+  // Update UI
+  updateCardsContainer();
+  updateNavigation();
 
-  card.innerHTML = `
-    <div class="inner-card">
-      <div class="inner-card-front">
-        <p>
-          ${data.question}
-        </p>
-      </div>
-      <div class="inner-card-back">
-        <p>
-          ${data.answer}
-        </p>
-      </div>
-    </div>
-  `;
-
-  card.addEventListener('click', () => {
-    card.classList.toggle('show-answer');
+  // Bind Events
+  nextBtn.addEventListener('click', () => {
+    if (state.activeCardIndex + 1 < state.cards.length) {
+      state.activeCardIndex++;
+      updateCard();
+      updateNavigation();
+    }
   });
 
-  // Add to DOM cards
-  cardsEl.push(card);
+  prevBtn.addEventListener('click', () => {
+    if (state.activeCardIndex - 1 >= 0) {
+      state.activeCardIndex--;
+      updateCard();
+      updateNavigation();
+    }
+  });
 
-  cardsContainer.appendChild(card);
+  showBtn.addEventListener('click', showAddContainer);
 
-  updateCurrentText();
+  hideBtn.addEventListener('click', hideAddContainer);
+
+  addCardBtn.addEventListener('click', submitNewCard);
+
+  clearBtn.addEventListener('click', clearCards);
 }
 
-// Show number of cards
-function updateCurrentText() {
-  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
-}
+init();
 
-// Get cards from local storage
-function getCardsData() {
-  const cards = JSON.parse(localStorage.getItem('cards'));
-  return cards === null ? [] : cards;
-}
-
-// Add cards to local storage
-function setCardsData(cards) {
-  localStorage.setItem('cards', JSON.stringify(cards));
-  window.location.reload();
-}
-
-createCards();
-
-// Event Listener
-nextBtn.addEventListener('click', () => {
-  cardsEl[currentActiveCard].className = 'card left';
-  currentActiveCard++;
-
-  if (currentActiveCard > cardsEl.length - 1) {
-    currentActiveCard--;
-  }
-
-  cardsEl[currentActiveCard].className = 'card active';
-
-  updateCurrentText();
-});
-
-prevBtn.addEventListener('click', () => {
-  cardsEl[currentActiveCard].className = 'card';
-  currentActiveCard--;
-
-  if (currentActiveCard < 0) {
-    currentActiveCard++;
-  }
-
-  cardsEl[currentActiveCard].className = 'card active';
-
-  updateCurrentText();
-});
-
-showBtn.addEventListener('click', () => {
-  addContainer.classList.add('show');
-});
-
-hideBtn.addEventListener('click', () => {
-  addContainer.classList.remove('show');
-});
-
-addCardBtn.addEventListener('click', () => {
-  const question = questionEl.value;
-  const answer = answerEl.value;
-
-  if (question.trim() && answer.trim()) {
-    const newCard = { question, answer };
-    createCard(newCard);
-
-    questionEl.value = '';
-    answerEl.value = '';
-
-    addContainer.classList.remove('show');
-
-    cardsData.push(newCard);
-    setCardsData(cardsData);
-  }
-});
-
-clearBtn.addEventListener('click', () => {
-  localStorage.removeItem('cards');
+function updateCardsContainer() {
   cardsContainer.innerHTML = '';
-  window.location.reload();
-});
+  if (state.cards.length > 0) {
+    state.cards.forEach((card, index) => {
+      const cardEl = document.createElement('div');
+
+      if (index === state.activeCardIndex) {
+        cardEl.className = 'card active';
+      } else if (index < state.activeCardIndex) {
+        cardEl.className = 'card left';
+      } else {
+        cardEl.className = 'card';
+      }
+
+      cardEl.innerHTML = `
+        <div class="inner-card">
+          <div class="inner-card-front">
+            <p>
+              ${card.question}
+            </p>
+          </div>
+          <div class="inner-card-back">
+            <p>
+              ${card.answer}
+            </p>
+          </div>
+        </div>
+      `;
+
+      cardEl.addEventListener('click', () => flipCard(index));
+
+      cardsContainer.appendChild(cardEl);
+    });
+  } else {
+    const emptyCardHTML = `
+      <div class="card active" onclick="showAddContainer()">
+        <div class="inner-card">
+          <div class="inner-card-front">
+            <p>
+              <span><i class="fas fa-plus"></i></span> Add New Card
+            </p>
+          </div>        
+      </div>
+    `;
+    cardsContainer.innerHTML = emptyCardHTML;
+  }
+}
+
+function updateCard() {
+  const cardsEl = cardsContainer.querySelectorAll('.card');
+  cardsEl.forEach((cardEl, index) => {
+    if (index === state.activeCardIndex) {
+      cardEl.className = 'card active';
+    } else if (index < state.activeCardIndex) {
+      cardEl.className = 'card left';
+    } else {
+      cardEl.className = 'card';
+    }
+  });
+}
+
+function updateNavigation() {
+  if (state.cards.length > 0) {
+    navigation.style.display = 'flex';
+    currentEl.innerText = `${state.activeCardIndex + 1}/${state.cards.length}`;
+  } else {
+    navigation.style.display = 'none';
+    currentEl.innerText = '';
+  }
+}
+
+function flipCard(index) {
+  const cardsEl = cardsContainer.querySelectorAll('.card');
+  cardsEl[index].classList.toggle('show-answer');
+}
+
+function showAddContainer() {
+  addContainer.classList.add('show');
+}
+
+function hideAddContainer() {
+  addContainer.classList.remove('show');
+}
+
+function submitNewCard() {
+  const newCard = {
+    question: questionEl.value,
+    answer: answerEl.value
+  };
+
+  state.cards.push(newCard);
+  state.activeCardIndex = state.cards.length - 1;
+
+  updateCardsContainer();
+  updateNavigation();
+
+  storeDataToLocalStorage();
+
+  hideAddContainer();
+  clearAddCard();
+}
+
+function clearAddCard() {
+  questionEl.value = '';
+  answerEl.value = '';
+}
+
+function storeDataToLocalStorage() {
+  localStorage.setItem('cards', JSON.stringify(state.cards));
+}
+
+function clearCards() {
+  if (window.confirm('Remove all cards?')) {
+    state.cards = [];
+    state.activeCardIndex = 0;
+    storeDataToLocalStorage();
+
+    updateCardsContainer();
+    updateNavigation();
+  }
+}
